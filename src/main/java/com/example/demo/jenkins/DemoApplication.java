fceba9bf18c6cc9d97b7995485ee67d4979a242a
siva <lsivakumar22@gmail.com>
2018-12-25 17:57:30 +0530

    Jenkins changed file

--- src/main/java/com/example/demo/jenkins/DemoApplication.java
+++ src/main/java/com/example/demo/jenkins/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo.jenkins;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,7 @@ public class DemoApplication {
 	
 	public static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	
+	@PostConstruct
 	public void init() {
 		logger.info("Application started...");
 	}
